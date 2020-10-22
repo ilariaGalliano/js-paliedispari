4c93375bdf58da95df430fcf544b51737e4d5d98
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-10-22 16:25:28 +0200

    js-paliedispari
    Esercizio 2

--- js/main.js
+++ js/main.js
@@ -4,30 +4,87 @@
 *
 */
 
-//Chiedere all'utente di inserire una parola
-var parola = prompt('Inserire una parola');
-console.log(parola);
+// //Chiedere all'utente di inserire una parola
+// var parola = prompt('Inserire una parola');
+// console.log(parola);
+//
+// // Definisco la funzione personalizzata
+// function reverseString(stringa) {
+//
+// }
+// // Uso la funzione personalizzata
+// var nuovaStringa = reverseString(parola);
+// console.log(nuovaStringa);
+//
+// //Creare una funzione per invertire i caratteri di una stringa.
+//   function reverseString(stringa) {
+//
+//   return stringa.split("").reverse().join("");
+// }
+//
+// // Capire se la parola inserita è palindroma
+// if(parola == nuovaStringa){
+//   console.log('La parola e\' palindroma');
+//   document.getElementById('text').innerHTML = 'La parola e\' palindroma';
+// }
+// else{
+//   console.log('La parola non e\' palindroma');
+//   document.getElementById('text').innerHTML = 'La parola non e\' palindroma';
+// }
 
-// definisco la funzione personalizzata
-function reverseString(stringa) {
+/*
+* Es. 2
+* Pari e Dispari
+*
+*/
 
+// L'utente sceglie pari o dispari
+var evenOdd = prompt('Pari o Dispari').toLowerCase();
+console.log(evenOdd);
+
+// L'utente inserisce un numero da 1 a 5
+var number = parseInt( prompt('Inserisci un numero da 1 a 5'));
+console.log(number);
+
+// Uso la funzione personalizzata
+var newString = genRandom(1,5);
+console.log(newString);
+
+// Generatore numeri random da 1 a 5 con una funzione
+function genRandom(min,max){
+
+  return Math.floor(Math.random() * (5 - 1 + 1) + 1);
 }
-// uso la funzione personalizzata
-var nuovaStringa = reverseString(parola);
-console.log(nuovaStringa);
 
-//Creare una funzione per invertire i caratteri di una stringa.
-  function reverseString(stringa) {
+// Sommiamo i due numeri
+var sum = number + newString;
+console.log('La somma dei due numeri è : ' + sum);
 
-  return stringa.split("").reverse().join("");
+// Uso la funzione personalizzata
+var final = sumEvenOdd(sum);
+//console.log(final);
+
+// Stabiliamo se la somma dei due numeri è pari o dispari con una funzione
+function sumEvenOdd(numEvenOdd) {
+
+  if (numEvenOdd % 2 == 0) {
+//    console.log('La somma dei due numeri è pari');
+    return 'La somma dei due numeri è pari';
+  }
+  else{
+  //  console.log('La somma dei due numeri è dispari');
+    return 'La somma dei due numeri è dispari';
+  }
+
+   return numEvenOdd;
 }
 
-// Capire se la parola inserita è palindroma
-if(parola == nuovaStringa){
-  console.log('La parola e\' palindroma');
-  document.getElementById('text').innerHTML = 'La parola e\' palindroma';
+//Dichiariamo chi ha vinto in base alla scelta pari o dispari.
+if (evenOdd == final) {
+  console.log('hai vinto');
+  document.getElementById('text').innerHTML = 'hai vinto';
 }
-else{
-  console.log('La parola non e\' palindroma');
-  document.getElementById('text').innerHTML = 'La parola non e\' palindroma';
+else {
+  console.log('hai perso');
+  document.getElementById('text').innerHTML = 'hai perso';
 }
