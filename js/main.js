24220c3c40631175276ad7bd2e7792c7bbf551a4
ilariaGalliano <70903186+ilariaGalliano@users.noreply.github.com>
2020-10-23 13:57:57 +0200

    js-paliedispari
    Completo

--- js/main.js
+++ js/main.js
@@ -4,33 +4,33 @@
 *
 */
 
-// //Chiedere all'utente di inserire una parola
-// var parola = prompt('Inserire una parola');
-// console.log(parola);
-//
-// // Definisco la funzione personalizzata
-// function reverseString(stringa) {
-//
-// }
-// // Uso la funzione personalizzata
-// var nuovaStringa = reverseString(parola);
-// console.log(nuovaStringa);
-//
-// //Creare una funzione per invertire i caratteri di una stringa.
-//   function reverseString(stringa) {
-//
-//   return stringa.split("").reverse().join("");
-// }
-//
-// // Capire se la parola inserita è palindroma
-// if(parola == nuovaStringa){
-//   console.log('La parola e\' palindroma');
-//   document.getElementById('text').innerHTML = 'La parola e\' palindroma';
-// }
-// else{
-//   console.log('La parola non e\' palindroma');
-//   document.getElementById('text').innerHTML = 'La parola non e\' palindroma';
-// }
+//Chiedere all'utente di inserire una parola
+var parola = prompt('Inserire una parola').toLowerCase;
+console.log(parola);
+
+// Definisco la funzione personalizzata
+function reverseString(stringa) {
+
+}
+// Uso la funzione personalizzata
+var nuovaStringa = reverseString(parola);
+console.log(nuovaStringa);
+
+//Creare una funzione per invertire i caratteri di una stringa.
+  function reverseString(stringa) {
+
+  return stringa.split("").reverse().join("");
+}
+
+// Capire se la parola inserita è palindroma
+if(parola == nuovaStringa){
+  console.log('La parola e\' palindroma');
+  document.getElementById('text').innerHTML = 'La parola e\' palindroma';
+}
+else{
+  console.log('La parola non e\' palindroma');
+  document.getElementById('text').innerHTML = 'La parola non e\' palindroma';
+}
 
 /*
 * Es. 2
@@ -39,21 +39,21 @@
 */
 
 // L'utente sceglie pari o dispari
-var evenOdd = prompt('Pari o Dispari').toLowerCase();
-console.log(evenOdd);
+var evenOdd = prompt('Pari o Dispari').toLowerCase().trim();
+console.log('Scelta giocatore: ' + evenOdd);
 
 // L'utente inserisce un numero da 1 a 5
 var number = parseInt( prompt('Inserisci un numero da 1 a 5'));
 console.log(number);
 
 // Uso la funzione personalizzata
-var newString = genRandom(1,5);
+var newString = genRandom(1, 5);
 console.log(newString);
 
 // Generatore numeri random da 1 a 5 con una funzione
 function genRandom(min,max){
 
-  return Math.floor(Math.random() * (5 - 1 + 1) + 1);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Sommiamo i due numeri
@@ -69,22 +69,20 @@ function sumEvenOdd(numEvenOdd) {
 
   if (numEvenOdd % 2 == 0) {
 //    console.log('La somma dei due numeri è pari');
-    return 'La somma dei due numeri è pari';
+    return 'pari';
   }
   else{
   //  console.log('La somma dei due numeri è dispari');
-    return 'La somma dei due numeri è dispari';
+    return 'dispari';
   }
-
-   return numEvenOdd;
 }
 
 //Dichiariamo chi ha vinto in base alla scelta pari o dispari.
-if (evenOdd == final) {
-  console.log('hai vinto');
-  document.getElementById('text').innerHTML = 'hai vinto';
+if (evenOdd === final) {
+  console.log('Hai vinto');
+  document.getElementById('text').innerHTML = 'Hai vinto';
 }
 else {
-  console.log('hai perso');
-  document.getElementById('text').innerHTML = 'hai perso';
+  console.log('Hai perso');
+  document.getElementById('text').innerHTML = 'Hai perso';
 }
